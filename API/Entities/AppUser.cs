fbdd676ef451ef3e68027a00b1caaade6305db32
mpalacz <mpalacz00@gmail.com>
2022-09-16 13:05:40 +0200

    Creating the Connection string

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,20 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace API.Entities
 {
     public class AppUser
     {
         public int Id { get; set; }
         public string UserName { get; set; }
-        private int myVar;
-        public int MyProperty
-        {
-            get { return myVar; }
-            set { myVar = value; }
-        }
-        
     }
 }
