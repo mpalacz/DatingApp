598a8b87d96e5dad38efb6982bf86070113d1714
mpalacz <mpalacz00@gmail.com>
2022-10-03 01:24:12 +0200

    Adding a Can Deactivate route guard

--- client/src/app/members/member-edit/member-edit.component.ts
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { take } from 'rxjs/operators';
@@ -16,9 +16,14 @@ export class MemberEditComponent implements OnInit {
   @ViewChild('editForm') editForm: NgForm;
   member: Member;
   user: User;
+  @HostListener('window:beforeunload', ['event']) unloadNotofication($event: any) {
+    if (this.editForm.dirty) {
+      $event.returnValue = true;
+    }
+  }
 
-  constructor(private accountService: AccountService, private memberService: MembersService, private toastr: ToastrService) { 
-      this.accountService.currentUser$.pipe(take(1)).subscribe(user => this.user = user);
+  constructor(private accountService: AccountService, private memberService: MembersService, private toastr: ToastrService) {
+    this.accountService.currentUser$.pipe(take(1)).subscribe(user => this.user = user);
   }
 
   ngOnInit(): void {
@@ -31,7 +36,7 @@ export class MemberEditComponent implements OnInit {
     })
   }
 
-  updateMember(){
+  updateMember() {
     console.log(this.member);
     this.toastr.success('Profile updated succesfully');
     this.editForm.reset(this.member);
