2897b4ad86fe734b12e7d33f535a657544889144
mpalacz <mpalacz00@gmail.com>
2022-11-04 12:37:24 +0100

    Configuring the DbContext

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -23,5 +23,6 @@ namespace API.Entities
 
         public ICollection<Message> MessagesSent { get; set; }
         public ICollection<Message> MessagesReceived { get; set; }
+        public ICollection<AppUserRole> UserRoles { get; set; }
     }
 }
