6fb82b6a84429d199ed8e196b99b702d59432d0c
mpalacz <mpalacz00@gmail.com>
2022-11-05 20:46:07 +0100

    Switch to http

--- API/Startup.cs
+++ API/Startup.cs
@@ -35,7 +35,7 @@ namespace API
 
             app.UseRouting();
 
-            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("https://localhost:4200"));
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("http://localhost:4200"));
 
             app.UseAuthentication();
 
